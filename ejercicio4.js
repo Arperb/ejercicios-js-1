44157a97fcf048a8aa6326b78905460f4e94ee1b
Arperb <72707444+Arperb@users.noreply.github.com>
2020-11-23 03:04:07 +0100

    Update ejercicio4.js
    No fui capaz de ver la infotmación de los characters en la URL, seguramente estoy poniendo mal la sintaxis para ver los campos

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,6 +1,6 @@
 // Try to show all the characters that are human by consuming the Rick and Morty API
 
-// Rick and Morty API:
+// Rick and Morty API
 // https://rickandmortyapi.com/documentation/#rest
 
 const axios = require ('axios');
